3f0a4ad61c61156224683f0c193408fc8a436b73
Geschan <git@misc.geschan.de>
2015-06-15 15:45:59 +0200

    Fix stupid bug

--- src/main/java/com/itextpdf/text/pdf/PdfPageLabels.java
+++ src/main/java/com/itextpdf/text/pdf/PdfPageLabels.java
@@ -180,9 +180,10 @@ public class PdfPageLabels {
 
 		int pagecount = 1;
 		Integer current;
+                String prefix;
 		char type = 'D';
 		for (int i = 0; i < n; i++) {
-                        String prefix = "";
+                        prefix = "";
 			current = Integer.valueOf(i);
 			if (numberTree.containsKey(current)) {
 				PdfDictionary d = (PdfDictionary)PdfReader.getPdfObjectRelease(numberTree.get(current));
